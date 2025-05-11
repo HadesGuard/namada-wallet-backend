a6d086266c509bc0690bb44def4f129d091f2b25
HadesGuard <truongquoctuan61@gmail.com>
2025-05-11 17:10:16 +0700

    add close redis connection

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { RedisService } from './redis/redis.service';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleDestroy {
+  constructor(private readonly redisService: RedisService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async onModuleDestroy() {
+    try {
+      // Close Redis connection
+      await this.redisService.onModuleDestroy();
+      console.log('Redis connection closed');
+    } catch (error) {
+      console.error('Error closing connections:', error);
+    }
   }
 }
